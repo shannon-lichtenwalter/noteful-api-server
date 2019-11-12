f54b3082416f5eb2dd8a1ac7ed9f3dec9e50e894
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-11-12 09:37:20 -0700

    all routes done, all tests done

---
+++ src/notes/notes-service.js
@@ -0,0 +1,40 @@
+const NotesService = {
+
+  getAllNotes(knex) {
+    return knex('notes')
+      .select('*');
+  },
+
+  insertNote(knex, noteData) {
+    return knex
+      .into('notes')
+      .insert(noteData)
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+
+  getNoteById(knex, id) {
+    return knex('notes')
+      .select('*')
+      .where({ id })
+      .first();
+  },
+
+  deleteNote(knex, id) {
+    return knex('notes')
+      .where({id})
+      .delete();
+  },
+
+  updateNote(knex, id, updatedNote) {
+    return knex('notes')
+      .where({id})
+      .update(updatedNote);
+  },
+
+};
+
+
+module.exports = NotesService;
